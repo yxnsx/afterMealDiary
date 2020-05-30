95abf0630bd76e539f24848c97aff098c24e5d83
yunso <cw2cu96@gmail.com>
2020-05-30 20:41:55 +0900

    NotificationService.class 파일에 ServiceThread, Notification, NotificationManager 객체 추가

--- app/src/main/java/com/example/aftermealdiary/NotificationService.java
+++ app/src/main/java/com/example/aftermealdiary/NotificationService.java
@@ -1,10 +1,17 @@
 package com.example.aftermealdiary;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
 public class NotificationService extends Service {
+
+    ServiceThread serviceThread;
+    Notification serviceNotification ;
+    NotificationManager notificationManager;
+
 
     public NotificationService() {
     }
