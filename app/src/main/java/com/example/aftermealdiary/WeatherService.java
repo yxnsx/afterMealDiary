5e4a5d669e7ccb6a892afeefbf691e5814ef7f3c
yunso <cw2cu96@gmail.com>
2020-05-31 22:40:30 +0900

    WeatherService.class에 서비스 구현에 필요한 메서드 오버라이드로 생성

--- app/src/main/java/com/example/aftermealdiary/WeatherService.java
+++ app/src/main/java/com/example/aftermealdiary/WeatherService.java
@@ -11,7 +11,20 @@ public class WeatherService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
 
-        
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 }
