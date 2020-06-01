57d54e9299f20d58bca0fa7701cca25b1cc8af55
yunso <cw2cu96@gmail.com>
2020-06-01 23:34:34 +0900

    AlarmService.class 파일에 mediaPlayer 추가

--- app/src/main/java/com/example/aftermealdiary/AlarmService.java
+++ app/src/main/java/com/example/aftermealdiary/AlarmService.java
@@ -2,9 +2,14 @@ package com.example.aftermealdiary;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 
 public class AlarmService extends Service {
+
+    MediaPlayer mediaPlayer;
+
+
     public AlarmService() {
     }
 
@@ -12,5 +17,23 @@ public class AlarmService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.alarm);
+        mediaPlayer.start();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 }
