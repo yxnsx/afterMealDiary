73c16a4886a9a05a98c58036f3a1014cea57f2e2
yunso <cw2cu96@gmail.com>
2020-06-02 04:11:18 +0900

    NutrientInfoActivity.class 파일에 레이아웃 리소스 선언

--- app/src/main/java/com/example/aftermealdiary/NutrientInfoActivity.java
+++ app/src/main/java/com/example/aftermealdiary/NutrientInfoActivity.java
@@ -3,12 +3,34 @@ package com.example.aftermealdiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class NutrientInfoActivity extends AppCompatActivity {
+public class NutrientInfoActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button imageButton_backArrow;
+    ListView listview_nutrientInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutrient_info);
+
+        imageButton_backArrow = findViewById(R.id.imageButton_backArrow);
+        listview_nutrientInfo = findViewById(R.id.listview_nutrientInfo);
+
+        imageButton_backArrow.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.imageButton_backArrow:
+                onBackPressed();
+        }
+
     }
 }
