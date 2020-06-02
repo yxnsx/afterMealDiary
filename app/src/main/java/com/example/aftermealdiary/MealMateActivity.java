f6ef0b285e6be27fd11f5bc9ff0ad235e84d1f4f
yunso <cw2cu96@gmail.com>
2020-06-02 22:18:38 +0900

    MealMateActivity.class 파일에 기기에서 연락처를 가져오는 getContact 메서드 구현

--- app/src/main/java/com/example/aftermealdiary/MealMateActivity.java
+++ app/src/main/java/com/example/aftermealdiary/MealMateActivity.java
@@ -1,17 +1,24 @@
 package com.example.aftermealdiary;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
+
 public class MealMateActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageButton imageButton_backArrow;
     ListView listView_contact;
 
+    Cursor cursor;
+    Cursor contactCursor;
+    HashMap<String, String> contactHashMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +38,30 @@ public class MealMateActivity extends AppCompatActivity implements View.OnClickL
             case R.id.imageButton_backArrow:
                 onBackPressed();
         }
+
+    }
+
+    private void getContact() {
+        cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, ContactsContract.Contacts.DISPLAY_NAME_PRIMARY);
+
+        while (cursor.moveToNext()) {
+            contactHashMap = new HashMap<String, String>();
+
+            String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY))
+
+            contactHashMap.put("name", name);
+
+            contactCursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id, null, null);
+
+            if (contactCursor.moveToFirst()) {
+                String contact = contactCursor.getString(contactCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                contactHashMap.put("contact", contact);
+            }
+
+            contactCursor.close();
+        }
+        cursor.close();
 
     }
 }
